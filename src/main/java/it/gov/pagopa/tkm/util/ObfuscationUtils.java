e676d73b21c2501dbd9a34264260abfc28e64fd5
David Bertoni <david.bertoni@gft.com>
2022-11-04 12:00:47 +0100

    PM-828

--- src/main/java/it/gov/pagopa/tkm/util/ObfuscationUtils.java
+++ src/main/java/it/gov/pagopa/tkm/util/ObfuscationUtils.java
@@ -1,37 +1,23 @@
 package it.gov.pagopa.tkm.util;
 
-import org.apache.commons.lang3.*;
-
 public class ObfuscationUtils {
 
     private ObfuscationUtils(){}
 
     public static String obfuscateHpan(String hpan) {
-        if (StringUtils.isBlank(hpan)) {
-            return hpan;
+        if (hpan != null && hpan.trim().length() > 6) {
+            hpan = hpan.trim();
+            hpan = hpan.substring(0, 6) + "************";
         }
-        return hpan.substring(0,3) + "***" + hpan.substring(hpan.length() - 3);
-    }
-
-    public static String obfuscateTaxCode(String taxCode) {
-        if (StringUtils.isBlank(taxCode)) {
-            return taxCode;
-        }
-        return taxCode.substring(0,3) + "***" + taxCode.substring(taxCode.length() - 3);
-    }
-
-    public static String obfuscatePar(String par) {
-        if (StringUtils.isBlank(par)) {
-            return par;
-        }
-        return par.substring(0,3) + "***" + par.substring(par.length() - 3);
+        return hpan;
     }
 
     public static String obfuscateHtoken(String htoken) {
-        if (StringUtils.isBlank(htoken)) {
-            return htoken;
-        }
-        return htoken.substring(0,3) + "***" + htoken.substring(htoken.length() - 3);
+        return obfuscateHpan(htoken);
+    }
+
+    public static String obfuscatePar(String par) {
+        return obfuscateHpan(par);
     }
 
 }
